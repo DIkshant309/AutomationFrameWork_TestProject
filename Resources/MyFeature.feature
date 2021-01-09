151be497a4a8576ae945641259b1e5891eaa09df
ShiviPrabhakar <58953132+ShiviPrabhakar@users.noreply.github.com>
2021-01-09 18:28:30 -0500

    testcommit
    testcommit

--- Resources/MyFeature.feature
+++ Resources/MyFeature.feature
@@ -1,6 +1,6 @@
 Feature: MyFeature_File
 
   Scenario: Test1
-    Given  landing on the facebook webpage
+    Given  landing on the twitter webpage
     When   entering username and password
     Then   validating that i am logged in
--- src/main/java/StepDefination/LoginTestStep.java
+++ src/main/java/StepDefination/LoginTestStep.java
@@ -1,5 +1,28 @@
 package StepDefination;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginTestStep {
+
+
+    @Given("landing on the twitter webpage")
+    public void landing_on_the_twitter_webpage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("entering username and password")
+    public void entering_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("validating that i am logged in")
+    public void validating_that_i_am_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
