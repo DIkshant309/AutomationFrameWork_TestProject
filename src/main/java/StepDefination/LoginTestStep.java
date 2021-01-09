6c289dc9d03f9260a17bd10cdda18dd752a98374
ShiviPrabhakar <58953132+ShiviPrabhakar@users.noreply.github.com>
2021-01-08 19:24:28 -0500

    Addition of the Test Steps
    Addition of the Test Steps

--- src/main/java/StepDefination/LoginTestStep.java
+++ src/main/java/StepDefination/LoginTestStep.java
@@ -1,5 +1,29 @@
 package StepDefination;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginTestStep {
+
+
+    @Given("landing on the facebook webpage")
+    public void landing_on_the_facebook_webpage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("entering username and password")
+    public void entering_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("validating that i am logged in")
+    public void validating_that_i_am_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 
 }
